82191300cc54c44a787be1f5139fb396da7263f3
mikrono <qse365@gmail.com>
2021-10-08 22:58:23 +0900

    Version 0.1 Added Bee Pistol

---
+++ Content/Items/Weapons/Summon/beePistol.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace iriesmod.Content.Items.Weapons.Summon
+{
+	public class beePistol : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Bee Pistol");
+			Tooltip.SetDefault("Shoots bees that will chase your enemy");
+		}
+
+		public override void SetDefaults()
+		{
+			item.damage = 5;
+			item.mana = 3;
+			item.summon = true;
+			item.width = 52;
+			item.height = 26;
+			item.useTime = 20;
+			item.useAnimation = 20;
+			item.useStyle = ItemUseStyleID.HoldingOut;
+			item.noMelee = true;
+			item.value = 10000;
+			item.rare = ItemRarityID.Blue;
+			item.UseSound = SoundID.Item11;
+			item.autoReuse = true;
+			item.shoot = ProjectileID.Bee;
+			item.shootSpeed = 8f;
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.FlintlockPistol);
+			recipe.AddIngredient(ItemID.Stinger, 15);
+			recipe.AddIngredient(ItemID.Hive, 8);
+			recipe.AddIngredient(ItemID.HoneyBlock, 8);
+			recipe.AddTile(TileID.Anvils);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+
+
+		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+		{
+			int numberProjectiles = 1 + Main.rand.Next(2);
+			for (int i = 0; i < numberProjectiles; i++)
+			{
+				Vector2 muzzleOffset = Vector2.Normalize(new Vector2(speedX, speedY - 1f)) * 51f;
+				if (Collision.CanHit(position, 0, 0, position + muzzleOffset, 0, 0))
+				{
+					position += muzzleOffset;
+				}
+				Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedByRandom(MathHelper.ToRadians(10));
+				float scale = 1f - (Main.rand.NextFloat() * .3f);
+				perturbedSpeed = perturbedSpeed * scale; 
+				Projectile.NewProjectile(position.X, position.Y, perturbedSpeed.X, perturbedSpeed.Y, type, damage, knockBack, player.whoAmI);
+			}
+			return false;
+		}
+	}
+}
---
+++ iriesmod.cs
@@ -0,0 +1,29 @@
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria;
+
+namespace iriesmod
+{
+	public class iriesmod : Mod
+	{
+
+
+		public override void AddRecipes()
+		{
+			RecipeFinder hornetstaffFinder = new RecipeFinder();
+			hornetstaffFinder.AddIngredient(ItemID.BeeWax, 14);
+			hornetstaffFinder.AddTile(TileID.Anvils);
+			hornetstaffFinder.SetResult(ItemID.HornetStaff);
+			Recipe hornetstaffRecipe = hornetstaffFinder.FindExactRecipe();
+			if (hornetstaffRecipe != null)
+			{
+				RecipeEditor editor = new RecipeEditor(hornetstaffRecipe);
+				editor.DeleteIngredient(ItemID.BeeWax);
+				editor.AddIngredient(ItemID.Stinger, 12);
+				editor.AddIngredient(ItemID.Hive, 15);
+				editor.AddIngredient(ItemID.HoneyBlock, 8);
+			}
+
+		}
+	}
+}
